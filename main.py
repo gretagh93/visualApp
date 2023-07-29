faa0b09c78df8bf324f14792d14fe656a7611bde
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-07-29 11:15:10 +0200

    Changes in structure

--- main.py
+++
@@ -1,5 +0,0 @@
-'''
-- Curs: https://course.fast.ai/Lessons/lesson1.html
-- Llibre que recolza aquest curs: https://github.com/fastai/fastbook/blob/master/01_intro.ipynb
-- https://www.deeplearning.ai/short-courses/
-'''
