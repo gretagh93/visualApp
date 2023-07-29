744aacb26f6cb57a92e7ceba86018eee8f1316f9
gretagh93 <48381971+gretagh93@users.noreply.github.com>
2023-07-29 10:59:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+'''
+- Curs: https://course.fast.ai/Lessons/lesson1.html
+- Llibre que recolza aquest curs: https://github.com/fastai/fastbook/blob/master/01_intro.ipynb
+- https://www.deeplearning.ai/short-courses/
+'''
